c9e1d7fccd6923558ed5b6edc041a6a9032ca25b
fabian nugrah <fabianugerah@gmail.com>
2026-02-09 19:09:29 +0700

    Update Design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     siteName: "CaffeeIn",
     images: [
       {
-        url: "/images/caffeein_small.png",
+        url: "/images/Preview.png",
         width: 1200,
         height: 630,
         alt: "CaffeeIn Logo",
